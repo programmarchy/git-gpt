ba6a9cae92da61358737ae8f601f7464da2fda48
Donald Ness <donald.ness@gmail.com>
2023-01-17 14:25:12 -0600

    Refactor setup.py to use pyproject.toml and README: Refactored setup.py to use pyproject.toml and README.rst for metadata, removed unnecessary imports and variables, updated entry point to reflect name change, and specified python version requirement in pyproject.toml. Updated git_gpt/__init__.py to remove irrelevant comments and rename __date__ variable for consistency.

--- git_gpt/__init__.py
+++ git_gpt/__init__.py
@@ -1,14 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: latin-1 -*-
-# Date Format: YYYY-MM-DD
+"""Generate commit messages using OpenAI GPT-3."""
 
-"""
-git_gpt:
-  Generate commit messages using OpenAI GPT-3
-"""
-
-__author__ = 'Donald Ness'
-__date__ = '2023-01-17'
-__version__ = '0.1.0'
+__version__ = "0.1.0"
 
 from .commit import commit
